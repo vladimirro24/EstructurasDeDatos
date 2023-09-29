cff74db36e1cc6e423e5fd7a6feff129f02c9a46
vladimirro24 <zamorarodrigo118@gmail.com>
2023-09-29 10:16:44 -0600

    unique value with set library

---
+++ ejerciciosC++/MergeLinkedLists.cpp
@@ -0,0 +1,17 @@
+//Problema:  Dadas dos listas enlazadas ordenadas. Unirlas en una sola lista ordenada.
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+int main(){
+
+    Node* list1 = nullptr;
+    Node* list2 = nullptr;
+
+    return 0;
+}
